f84ad5f31292fa75f13c299d017470c7cbaedabc
Michael <michaelyoung1995@gmail.com>
2014-03-27 02:34:55 -0400

    updated

--- app.js
+++ app.js
@@ -9,15 +9,8 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path')
-  , redis =  require('redis');
+  , path = require('path');
   
-
-
-var db = redis.createClient();
-db.on("error",function(err){
-	console.error("Error connecting to redis", err);
-});
 
 
 var app = express();
@@ -37,31 +30,6 @@ case 'development':
 default: cur_port = 3000;
 }
 
-function addUser(req, res, next){
-	
-  var ua = req.headers['user-agent'];
-  db.zadd('online', Date.now(), ua, next);
-  
-}
-
-//fetch the users online in the last minute
-function getCurUsers(req, res, next){
-  var min = 60 * 1000;
-  var ago = Date.now() - min;
-  db.zrevrangebyscore('online', '+inf', ago, function(err, users){
-    if (err) return next(err);
-    req.curUsers = users;
-    next();
-  });
-}
-	 
-function getTotalUsers(req, res, next){
-  db.zrevrangebyscore('online', '+inf', 0, function(err, users){
-    if (err) return next(err);
-    req.totalUsers = users;
-    next();
-  });
-}
 	 
 
 function compile(str, path) {
@@ -77,11 +45,6 @@ app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.bodyParser());
 app.use(express.methodOverride());
- 
-app.use(addUser);
-app.use(getCurUsers);
-app.use(getTotalUsers);
-
 
 app.use(app.router);
 app.use(stylus.middleware(
@@ -94,7 +57,7 @@ app.use(stylus.middleware(
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+
 
 app.use(routes.notFound);
 app.use(routes.errorPage);
